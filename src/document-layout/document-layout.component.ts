994419ba392f2dc2662a08323aa0a75d4b0b7d24
dvoikinalex <94dvoikin@gmail.com>
2017-10-09 19:48:26 +0300

    [Document layoyt] add as component

---
+++ src/document-layout/document-layout.component.ts
@@ -0,0 +1,23 @@
+import { Component, Renderer, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
+
+@Component({ 
+    selector: 'pip-document',
+    styleUrls: ['./document-layout.component.scss'],
+    template: '<div class="pip-document-content"><ng-content></ng-content></div>'
+})
+export class PipDocumentLayoutComponent implements OnInit, OnDestroy {
+    constructor(
+        private renderer: Renderer,
+        private elRef: ElementRef
+    ) {
+        renderer.setElementClass(elRef.nativeElement, 'pip-document', true);
+    }
+
+    public ngOnInit() {
+        
+    }
+
+    public ngOnDestroy() {
+        
+    }
+}
---
+++ src/document-layout/document-layout.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { PipDocumentLayoutComponent } from './document-layout.component';
+
+@NgModule({
+  declarations: [
+    PipDocumentLayoutComponent
+  ],
+  imports: [
+    
+  ],
+  exports: [  
+    PipDocumentLayoutComponent
+  ],
+  providers: [
+    PipDocumentLayoutComponent
+  ]
+})
+export class PipDocumentLayoutModule { }
