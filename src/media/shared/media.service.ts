d722644f43fd66267b1b504221d0f91e69b99e0c
dvoikinalex <94dvoikin@gmail.com>
2017-10-10 16:33:38 +0300

    [Media service] update activate func

--- src/media/shared/media.service.ts
+++ src/media/shared/media.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-import {MediaChange, ObservableMedia} from "@angular/flex-layout";
+import { BREAKPOINTS, MediaChange, ObservableMedia } from "@angular/flex-layout";
 
 @Injectable()
 export class PipMediaService {
@@ -11,32 +11,11 @@ export class PipMediaService {
         private media: ObservableMedia
     ) { }
 
-    private classes: any = {
-        xs: ['pip-global-xs'],
-        sm: ['pip-global-gt-xs', 'pip-global-sm'],
-        md: ['pip-global-gt-xs', 'pip-global-gt-sm', 'pip-global-md'],
-        lg: ['pip-global-gt-xs', 'pip-global-gt-sm', 'pip-global-gt-md', 'pip-global-lg'],
-        xl: ['pip-global-gt-xs', 'pip-global-gt-sm', 'pip-global-gt-md', 'pip-global-gt-lg', 'pip-global-xl'],
-        all: [
-            'pip-global-xs', 
-            'pip-global-gt-xs', 
-            'pip-global-sm', 
-            'pip-global-gt-sm', 
-            'pip-global-md',
-            'pip-global-gt-md', 
-            'pip-global-lg',
-            'pip-global-lg', 
-            'pip-global-gt-lg', 
-            'pip-global-xl'
-        ]
-    };
-
     public activate() {
         this.media.asObservable().subscribe((change: MediaChange) => {
             let body = document.body;
-            let currentClasses: string[] = this.classes[change.mqAlias];
-            _.each(this.classes.all, (name: any) => {
-                body.classList[currentClasses.includes(name) ? 'add' : 'remove'](name);
+            _.each(this.media['breakpoints'].aliases, (alias: any) => {
+                body.classList[this.media.isActive(alias) ? 'add' : 'remove']('pip-' + alias);
             });
         });
     }
