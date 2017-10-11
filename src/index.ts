9d509e31c3ed8aaa0ff2417ae79fbfcafdad0ed0
dvoikinalex <94dvoikin@gmail.com>
2017-10-11 14:02:41 +0300

    [Index] update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './test/index';
 
 export * from './document-layout/index'; 
 export * from './main-layout/index';
+export * from './tiles-layout/index';
 export * from './media/index';
