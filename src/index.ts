62b4adfd728bd4ba7b060b9fd5b839e0f7999edc
dvoikinalex <94dvoikin@gmail.com>
2017-10-12 19:03:51 +0300

    [Root index] add exports

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export * from './document-layout/index';
 export * from './main-layout/index';
 export * from './tiles-layout/index';
 export * from './media/index';
+export * from './appbar/index';
+export * from './sidenav/index';
+export * from './content-layout/index';
