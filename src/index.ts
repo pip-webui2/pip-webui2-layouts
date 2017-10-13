e1ba128ebc3f5fbc4c2071a2c9fddda0491351b3
dvoikinalex <94dvoikin@gmail.com>
2017-10-13 16:43:43 +0300

    [Root index]

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ export * from './tiles-layout/index';
 export * from './media/index';
 export * from './appbar/index';
 export * from './sidenav/index';
+export * from './aux-panel/index';
 export * from './content-layout/index';
