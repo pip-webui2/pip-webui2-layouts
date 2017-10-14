321f84adda50be519054fb0f8e1a1bcdb2901e1e
dvoikinalex <94dvoikin@gmail.com>
2017-10-14 12:52:50 +0300

    [Media] update

--- src/media/index.ts
+++ src/media/index.ts
@@ -1,3 +1,4 @@
 export { PipMediaModule } from './media.module';
 export { PipMediaService } from './shared/media.service';
+export { MediaMainChange } from './shared/media-main-change.model';
 export * from './resize-layout.function';
