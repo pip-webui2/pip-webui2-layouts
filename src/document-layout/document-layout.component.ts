fb28452f89b7bfd1f7be0b4c165bf19b3a259ba4
Алексей Двойкин <94dvoikin@gmail.com>
2017-11-01 19:14:32 +0300

    [Document layout] add toolbar as attribute

--- src/document-layout/document-layout.component.ts
+++ src/document-layout/document-layout.component.ts
@@ -6,6 +6,10 @@ import { Component, Renderer, ElementRef, Input, OnInit, OnDestroy } from '@angu
     template: '<div class="pip-document-content"><ng-content></ng-content></div>'
 })
 export class PipDocumentLayoutComponent implements OnInit, OnDestroy {
+    @Input() set toolbar(toolbar: boolean | string) {
+        this.elRef.nativeElement.classList[toolbar === 'true' || toolbar === true ? 'add' : 'remove']('pip-with-toolbar');
+    }
+
     constructor(
         private renderer: Renderer,
         private elRef: ElementRef
