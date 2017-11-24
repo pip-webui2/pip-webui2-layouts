55400717e78f1ef6fdd37caf902f7dcf0c6a6a39
Алексей Двойкин <94dvoikin@gmail.com>
2017-11-24 18:08:32 +0300

    [Media] add breakpoints

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
+import { BREAKPOINTS } from '@angular/flex-layout';
 
 import { PipMediaService } from './shared/media.service';
 
+import { PIP_BREAKPOINTS } from './shared/breakpoints';
+
+export const PipBreakPointsProvider = {
+  provide: BREAKPOINTS,
+  useValue: [...PIP_BREAKPOINTS]
+};
+
 @NgModule({
   declarations: [
-    
+
   ],
   imports: [
-    
+
   ],
   exports: [
-    
+
   ],
   providers: [
-    PipMediaService
+    PipMediaService,
+    PipBreakPointsProvider
   ]
 })
 export class PipMediaModule { }
