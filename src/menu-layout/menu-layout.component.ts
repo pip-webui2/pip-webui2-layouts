a18629fa98d04ba1ef4af00cd1ddead3147f5923
Алексей Двойкин <94dvoikin@gmail.com>
2017-12-22 15:46:23 +0300

    [Menu layout] add single input

--- src/menu-layout/menu-layout.component.ts
+++ src/menu-layout/menu-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Renderer, ElementRef, Input, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'pip-menu-layout',
@@ -6,9 +6,17 @@ import { Component, Renderer, ElementRef, Input, OnInit, OnDestroy } from '@angu
     template: '<ng-content></ng-content>'
 })
 export class PipMenuLayoutComponent implements OnInit, OnDestroy {
+    @Input() set single(single: boolean) {
+        this.elRef.nativeElement.classList[single ? 'add' : 'remove']('pip-single-content');
+        if (this) {
+            this.cd.detectChanges();
+        }
+    }
+
     constructor(
         private renderer: Renderer,
-        private elRef: ElementRef
+        private elRef: ElementRef,
+        private cd: ChangeDetectorRef
     ) {
         renderer.setElementClass(elRef.nativeElement, 'pip-menu-layout', true);
     }
