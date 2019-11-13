4793bd943baf94863c8a3858c4239249becccab9
Aleksandr Koshevierov <iworb.ne@gmail.com>
2019-11-13 13:59:34 +0300

    fix in example

--- src/app/tabs-example/tabs-example.component.ts
+++ src/app/tabs-example/tabs-example.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import cloneDeep from 'lodash/cloneDeep';
+import { cloneDeep } from 'lodash';
 import { PipAppbarService, PipAppbarTab } from 'pip-webui2-layouts';
 
 @Component({
